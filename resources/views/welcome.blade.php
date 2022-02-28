abf05def88438f8bff74069881625e999942784d
Naufal Fachrezi <49073262+hosefg@users.noreply.github.com>
2022-02-28 16:52:12 +0700

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,12 +22,12 @@
         </style> -->
     </head>
     <body class="antialiased">
-        <header class=" bg-gray-900">
-            <nav class=" container flex items-center bg-gray-900 py-9 mx-auto">
+        <header class=" bg-zinc-800">
+            <nav class=" container flex items-center bg-zinc-800 py-9 mx-auto">
                 <div class=" w-52 ml-5 py-1">
                     <img src="https://cdn.discordapp.com/attachments/711190437922209833/946645863227359283/unknown.png" alt="">
                 </div>
-                <ul class="hidden sm:flex flex-1 justify-end items-center gap-7 text-gray-50 text-base mr-5 font-sans">
+                <ul class="hidden md:flex flex-1 justify-end items-center gap-7 text-gray-50 text-base mr-5 font-sans">
                     <li class="cursor-pointer hover:text-orange-500">Home</li>
                     <li class="cursor-pointer hover:text-orange-500">Jobs</li>
                     <button class=" bg-orange-500 hover:bg-orange-700 text-white rounded-md p-2 font-sans">Login</button>
@@ -35,13 +35,231 @@
             </nav>
         </header>
         <div class=" container-2xl flex items-center bg-zinc-600 mx-auto">
-            <div class=" flex items-center mx-44 my-28">
-                <img class="flex items-center"src="https://cdn.discordapp.com/attachments/711190437922209833/946664293779243048/unknown.png" alt="">
+            <div class=" flex-initial items-center px-44 my-28">
+                <img class="items-center" src="https://cdn.discordapp.com/attachments/711190437922209833/946664293779243048/unknown.png" alt="">
             </div>
             <div class=" flex items-center">
                 <img class="" src="https://cdn.discordapp.com/attachments/711190437922209833/946664774991753256/unknown.png" alt="">
             </div>
             </div>
         </div>
+        <div class=" container-2xl md:flex justify-center items-center bg-zinc-800 mx-auto">
+            <div class="items-center mt-9 mb-3 md:flex justify-center">
+                <input class=" placeholder:text-white bg-zinc-400 rounded-md text-white py-1 px-2 w-64 shadow-md" type="text" placeholder="Search...">
+            </div>
+        </div>
+        <div class="container-2xl md:flex justify-center items-center bg-zinc-800 mx-auto">
+            <ul class="hidden md:flex flex-1 justify-center items-center gap-7 text-gray-50 text-base mr-5 font-sans">
+                <button class=" hover:bg-orange-500 bg-transparent text-white rounded-md p-2 font-sans">All</button>
+                <button class=" hover:bg-orange-500 bg-transparent text-white rounded-md p-2 font-sans">Images</button>
+                <button class=" hover:bg-orange-500 bg-transparent text-white rounded-md p-2 font-sans">Video</button>
+                <button class=" hover:bg-orange-500 bg-transparent text-white rounded-md p-2 font-sans">Audio</button>
+                <button class=" hover:bg-orange-500 bg-transparent text-white rounded-md p-2 font-sans">Script</button>                
+            </ul>
+        </div>
+        <div class="container-2xl md:flex justify-center items-center bg-zinc-800 mx-auto px-12 space-x-12 py-5">
+            <div class="container bg-zinc-700 rounded-lg shadow-lg p-4">
+                <div class="container md:flex items-center">
+                    <img class="rounded-full float-left w-12 h-12" src="https://img.freepik.com/free-photo/mand-holding-cup_1258-340.jpg?size=626&ext=jpg&ga=GA1.2.1546389280.1639353600" alt="">
+                    <div class="px-3.5">
+                        <p class="font-semibold font-sans text-white">Fajar</p>
+                        <p class="font-thin font-sans text-orange-500">Images</p>
+                    </div>
+                </div>
+                
+                <div class="container relative overflow-hidden py-2">
+                    <img src="https://img.freepik.com/free-photo/mand-holding-cup_1258-340.jpg?size=626&ext=jpg&ga=GA1.2.1546389280.1639353600" alt="" class="object-cover w-full h-full rounded-md" />
+                    <div class="absolute py-2.5 bottom-2 inset-x-0 opacity-80 bg-black text-white text-xs text-center leading-4 rounded-b-md font-sans font-medium">Abstract Mode</div>
+                </div>
+                <div class="container md:flex justify-end items-center">
+                    <img class="w-5" src="https://cdn.discordapp.com/attachments/752542767128772668/947775687748616202/unknown.png" alt="">
+                    <p class="px-2 text-amber-100">202</p>
+                    <img class="w-5" src="https://cdn.discordapp.com/attachments/752542767128772668/947777319311904768/unknown.png" alt="">
+                    <p class="px-2 text-amber-100">61.5 K</p>
+                </div>
+            </div>
+            <div class="container bg-zinc-700 rounded-lg shadow-lg p-4">
+                <div class="container md:flex items-center">
+                    <img class="rounded-full float-left w-12 h-12" src="https://img.freepik.com/free-photo/mand-holding-cup_1258-340.jpg?size=626&ext=jpg&ga=GA1.2.1546389280.1639353600" alt="">
+                    <div class="px-3.5">
+                        <p class="font-semibold font-sans text-white">Fajar</p>
+                        <p class="font-thin font-sans text-orange-500">Images</p>
+                    </div>
+                </div>
+                
+                <div class="container relative overflow-hidden py-2">
+                    <img src="https://img.freepik.com/free-photo/mand-holding-cup_1258-340.jpg?size=626&ext=jpg&ga=GA1.2.1546389280.1639353600" alt="" class="object-cover w-full h-full rounded-md" />
+                    <div class="absolute py-2.5 bottom-2 inset-x-0 opacity-80 bg-black text-white text-xs text-center leading-4 rounded-b-md font-sans font-medium">Abstract Mode</div>
+                </div>
+                <div class="container md:flex justify-end items-center">
+                    <img class="w-5" src="https://cdn.discordapp.com/attachments/752542767128772668/947775687748616202/unknown.png" alt="">
+                    <p class="px-2 text-amber-100">202</p>
+                    <img class="w-5" src="https://cdn.discordapp.com/attachments/752542767128772668/947777319311904768/unknown.png" alt="">
+                    <p class="px-2 text-amber-100">61.5 K</p>
+                </div>
+            </div>
+            <div class="container bg-zinc-700 rounded-lg shadow-lg p-4">
+                <div class="container md:flex items-center">
+                    <img class="rounded-full float-left w-12 h-12" src="https://img.freepik.com/free-photo/mand-holding-cup_1258-340.jpg?size=626&ext=jpg&ga=GA1.2.1546389280.1639353600" alt="">
+                    <div class="px-3.5">
+                        <p class="font-semibold font-sans text-white">Fajar</p>
+                        <p class="font-thin font-sans text-orange-500">Images</p>
+                    </div>
+                </div>
+                
+                <div class="container relative overflow-hidden py-2">
+                    <img src="https://img.freepik.com/free-photo/mand-holding-cup_1258-340.jpg?size=626&ext=jpg&ga=GA1.2.1546389280.1639353600" alt="" class="object-cover w-full h-full rounded-md" />
+                    <div class="absolute py-2.5 bottom-2 inset-x-0 opacity-80 bg-black text-white text-xs text-center leading-4 rounded-b-md font-sans font-medium">Abstract Mode</div>
+                </div>
+                <div class="container md:flex justify-end items-center">
+                    <img class="w-5" src="https://cdn.discordapp.com/attachments/752542767128772668/947775687748616202/unknown.png" alt="">
+                    <p class="px-2 text-amber-100">202</p>
+                    <img class="w-5" src="https://cdn.discordapp.com/attachments/752542767128772668/947777319311904768/unknown.png" alt="">
+                    <p class="px-2 text-amber-100">61.5 K</p>
+                </div>
+            </div>
+            <div class="container bg-zinc-700 rounded-lg shadow-lg p-4">
+                <div class="container md:flex items-center">
+                    <img class="rounded-full float-left w-12 h-12" src="https://img.freepik.com/free-photo/mand-holding-cup_1258-340.jpg?size=626&ext=jpg&ga=GA1.2.1546389280.1639353600" alt="">
+                    <div class="px-3.5">
+                        <p class="font-semibold font-sans text-white">Fajar</p>
+                        <p class="font-thin font-sans text-orange-500">Images</p>
+                    </div>
+                </div>
+                
+                <div class="container relative overflow-hidden py-2">
+                    <img src="https://img.freepik.com/free-photo/mand-holding-cup_1258-340.jpg?size=626&ext=jpg&ga=GA1.2.1546389280.1639353600" alt="" class="object-cover w-full h-full rounded-md" />
+                    <div class="absolute py-2.5 bottom-2 inset-x-0 opacity-80 bg-black text-white text-xs text-center leading-4 rounded-b-md font-sans font-medium">Abstract Mode</div>
+                </div>
+                <div class="container md:flex justify-end items-center">
+                    <img class="w-5" src="https://cdn.discordapp.com/attachments/752542767128772668/947775687748616202/unknown.png" alt="">
+                    <p class="px-2 text-amber-100">202</p>
+                    <img class="w-5" src="https://cdn.discordapp.com/attachments/752542767128772668/947777319311904768/unknown.png" alt="">
+                    <p class="px-2 text-amber-100">61.5 K</p>
+                </div>
+            </div>
+            <div class="container bg-zinc-700 rounded-lg shadow-lg p-4">
+                <div class="container md:flex items-center">
+                    <img class="rounded-full float-left w-12 h-12" src="https://img.freepik.com/free-photo/mand-holding-cup_1258-340.jpg?size=626&ext=jpg&ga=GA1.2.1546389280.1639353600" alt="">
+                    <div class="px-3.5">
+                        <p class="font-semibold font-sans text-white">Fajar</p>
+                        <p class="font-thin font-sans text-orange-500">Images</p>
+                    </div>
+                </div>
+                
+                <div class="container relative overflow-hidden py-2">
+                    <img src="https://img.freepik.com/free-photo/mand-holding-cup_1258-340.jpg?size=626&ext=jpg&ga=GA1.2.1546389280.1639353600" alt="" class="object-cover w-full h-full rounded-md" />
+                    <div class="absolute py-2.5 bottom-2 inset-x-0 opacity-80 bg-black text-white text-xs text-center leading-4 rounded-b-md font-sans font-medium">Abstract Mode</div>
+                </div>
+                <div class="container md:flex justify-end items-center">
+                    <img class="w-5" src="https://cdn.discordapp.com/attachments/752542767128772668/947775687748616202/unknown.png" alt="">
+                    <p class="px-2 text-amber-100">202</p>
+                    <img class="w-5" src="https://cdn.discordapp.com/attachments/752542767128772668/947777319311904768/unknown.png" alt="">
+                    <p class="px-2 text-amber-100">61.5 K</p>
+                </div>
+            </div>   
+        </div>
+        <div class="container-2xl md:flex justify-center items-center bg-zinc-800 mx-auto px-12 space-x-12 py-5">
+            <div class="container bg-zinc-700 rounded-lg shadow-lg p-4">
+                <div class="container md:flex items-center">
+                    <img class="rounded-full float-left w-12 h-12" src="https://img.freepik.com/free-photo/mand-holding-cup_1258-340.jpg?size=626&ext=jpg&ga=GA1.2.1546389280.1639353600" alt="">
+                    <div class="px-3.5">
+                        <p class="font-semibold font-sans text-white">Fajar</p>
+                        <p class="font-thin font-sans text-orange-500">Images</p>
+                    </div>
+                </div>
+                
+                <div class="container relative overflow-hidden py-2">
+                    <img src="https://img.freepik.com/free-photo/mand-holding-cup_1258-340.jpg?size=626&ext=jpg&ga=GA1.2.1546389280.1639353600" alt="" class="object-cover w-full h-full rounded-md" />
+                    <div class="absolute py-2.5 bottom-2 inset-x-0 opacity-80 bg-black text-white text-xs text-center leading-4 rounded-b-md font-sans font-medium">Abstract Mode</div>
+                </div>
+                <div class="container md:flex justify-end items-center">
+                    <img class="w-5" src="https://cdn.discordapp.com/attachments/752542767128772668/947775687748616202/unknown.png" alt="">
+                    <p class="px-2 text-amber-100">202</p>
+                    <img class="w-5" src="https://cdn.discordapp.com/attachments/752542767128772668/947777319311904768/unknown.png" alt="">
+                    <p class="px-2 text-amber-100">61.5 K</p>
+                </div>
+            </div>
+            <div class="container bg-zinc-700 rounded-lg shadow-lg p-4">
+                <div class="container md:flex items-center">
+                    <img class="rounded-full float-left w-12 h-12" src="https://img.freepik.com/free-photo/mand-holding-cup_1258-340.jpg?size=626&ext=jpg&ga=GA1.2.1546389280.1639353600" alt="">
+                    <div class="px-3.5">
+                        <p class="font-semibold font-sans text-white">Fajar</p>
+                        <p class="font-thin font-sans text-orange-500">Images</p>
+                    </div>
+                </div>
+                
+                <div class="container relative overflow-hidden py-2">
+                    <img src="https://img.freepik.com/free-photo/mand-holding-cup_1258-340.jpg?size=626&ext=jpg&ga=GA1.2.1546389280.1639353600" alt="" class="object-cover w-full h-full rounded-md" />
+                    <div class="absolute py-2.5 bottom-2 inset-x-0 opacity-80 bg-black text-white text-xs text-center leading-4 rounded-b-md font-sans font-medium">Abstract Mode</div>
+                </div>
+                <div class="container md:flex justify-end items-center">
+                    <img class="w-5" src="https://cdn.discordapp.com/attachments/752542767128772668/947775687748616202/unknown.png" alt="">
+                    <p class="px-2 text-amber-100">202</p>
+                    <img class="w-5" src="https://cdn.discordapp.com/attachments/752542767128772668/947777319311904768/unknown.png" alt="">
+                    <p class="px-2 text-amber-100">61.5 K</p>
+                </div>
+            </div>
+            <div class="container bg-zinc-700 rounded-lg shadow-lg p-4">
+                <div class="container md:flex items-center">
+                    <img class="rounded-full float-left w-12 h-12" src="https://img.freepik.com/free-photo/mand-holding-cup_1258-340.jpg?size=626&ext=jpg&ga=GA1.2.1546389280.1639353600" alt="">
+                    <div class="px-3.5">
+                        <p class="font-semibold font-sans text-white">Fajar</p>
+                        <p class="font-thin font-sans text-orange-500">Images</p>
+                    </div>
+                </div>
+                
+                <div class="container relative overflow-hidden py-2">
+                    <img src="https://img.freepik.com/free-photo/mand-holding-cup_1258-340.jpg?size=626&ext=jpg&ga=GA1.2.1546389280.1639353600" alt="" class="object-cover w-full h-full rounded-md" />
+                    <div class="absolute py-2.5 bottom-2 inset-x-0 opacity-80 bg-black text-white text-xs text-center leading-4 rounded-b-md font-sans font-medium">Abstract Mode</div>
+                </div>
+                <div class="container md:flex justify-end items-center">
+                    <img class="w-5" src="https://cdn.discordapp.com/attachments/752542767128772668/947775687748616202/unknown.png" alt="">
+                    <p class="px-2 text-amber-100">202</p>
+                    <img class="w-5" src="https://cdn.discordapp.com/attachments/752542767128772668/947777319311904768/unknown.png" alt="">
+                    <p class="px-2 text-amber-100">61.5 K</p>
+                </div>
+            </div>
+            <div class="container bg-zinc-700 rounded-lg shadow-lg p-4">
+                <div class="container md:flex items-center">
+                    <img class="rounded-full float-left w-12 h-12" src="https://img.freepik.com/free-photo/mand-holding-cup_1258-340.jpg?size=626&ext=jpg&ga=GA1.2.1546389280.1639353600" alt="">
+                    <div class="px-3.5">
+                        <p class="font-semibold font-sans text-white">Fajar</p>
+                        <p class="font-thin font-sans text-orange-500">Images</p>
+                    </div>
+                </div>
+                
+                <div class="container relative overflow-hidden py-2">
+                    <img src="https://img.freepik.com/free-photo/mand-holding-cup_1258-340.jpg?size=626&ext=jpg&ga=GA1.2.1546389280.1639353600" alt="" class="object-cover w-full h-full rounded-md" />
+                    <div class="absolute py-2.5 bottom-2 inset-x-0 opacity-80 bg-black text-white text-xs text-center leading-4 rounded-b-md font-sans font-medium">Abstract Mode</div>
+                </div>
+                <div class="container md:flex justify-end items-center">
+                    <img class="w-5" src="https://cdn.discordapp.com/attachments/752542767128772668/947775687748616202/unknown.png" alt="">
+                    <p class="px-2 text-amber-100">202</p>
+                    <img class="w-5" src="https://cdn.discordapp.com/attachments/752542767128772668/947777319311904768/unknown.png" alt="">
+                    <p class="px-2 text-amber-100">61.5 K</p>
+                </div>
+            </div>
+            <div class="container bg-zinc-700 rounded-lg shadow-lg p-4">
+                <div class="container md:flex items-center">
+                    <img class="rounded-full float-left w-12 h-12" src="https://img.freepik.com/free-photo/mand-holding-cup_1258-340.jpg?size=626&ext=jpg&ga=GA1.2.1546389280.1639353600" alt="">
+                    <div class="px-3.5">
+                        <p class="font-semibold font-sans text-white">Fajar</p>
+                        <p class="font-thin font-sans text-orange-500">Images</p>
+                    </div>
+                </div>
+                
+                <div class="container relative overflow-hidden py-2">
+                    <img src="https://img.freepik.com/free-photo/mand-holding-cup_1258-340.jpg?size=626&ext=jpg&ga=GA1.2.1546389280.1639353600" alt="" class="object-cover w-full h-full rounded-md" />
+                    <div class="absolute py-2.5 bottom-2 inset-x-0 opacity-80 bg-black text-white text-xs text-center leading-4 rounded-b-md font-sans font-medium">Abstract Mode</div>
+                </div>
+                <div class="container md:flex justify-end items-center">
+                    <img class="w-5" src="https://cdn.discordapp.com/attachments/752542767128772668/947775687748616202/unknown.png" alt="">
+                    <p class="px-2 text-amber-100">202</p>
+                    <img class="w-5" src="https://cdn.discordapp.com/attachments/752542767128772668/947777319311904768/unknown.png" alt="">
+                    <p class="px-2 text-amber-100">61.5 K</p>
+                </div>
+            </div>
+            
     </body>
 </html>
